d5f007ab53609e3d3011a514f3c6e501c3f41178
wangkj <wangkj@linkcld.com>
2018-09-11 19:47:35 +0800

    获取图片缩略图

---
+++ src/main/java/com/example/springbootwebdemo/pay/alipay/service/AliPayService.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebdemo.pay.alipay.service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public interface AliPayService {
+
+    Map payOrder(BigDecimal tradeMoney, String orderNumber, String timeoutExpress);
+
+    Map refundOrder(String refundMoney, String orderNumber, String ticketId);
+
+    Map refundStatus(String orderNumber, String ticketId);
+}
